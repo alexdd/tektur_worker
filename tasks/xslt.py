05c3f39fa4b7e625f38d5485a2d9ed8699bd0c68
Alex Düsel <tekturcms@gmail.com>
2020-02-09 18:05:34 +0100

    testing batch convert xml

--- tasks/xslt.py
+++ tasks/xslt.py
@@ -1,4 +1,4 @@
-import os, tempfile, glob
+import os, tempfile
 from subprocess import Popen, PIPE
 from settings import SAXON_JAR
 from errors import TaskError
@@ -66,30 +66,25 @@ def batch_transform_xml(process_dir, variables):
         variables -- a dictonary containing key-value pairs passed from Camunda
     Camunda Parameters:
         ["xslt-folder"] -- The name of the folder containing the XSLT files to process in sort order
-        ["input-folder"] -- The name of the folder containing the input files; 
-                            THe folder must have subfolders containing the input files
+        ["input-folder"] -- The name of the folder containing the input files 
         ["output-folder"] -- The name of the target folder; input folder structure will be replicated
         ["parameters"] -- a dictionary containing parameter settings passed to the transformation
     """
 
     outputf = os.path.join(process_dir, variables["output-folder"]["value"])
-    inputf = os.path.join(process_dir, variables["input-folder"]["value"])
+    input_folder = os.path.join(process_dir, variables["input-folder"]["value"])
     xslt_folder = os.path.join(process_dir, variables["xslt-folder"]["value"])
     os.mkdir(outputf)
-    glob_path = os.path.join(inputf,'*')
-    for fpath in glob.glob(glob_path): 
-        if os.path.isdir(fpath):
+    for file_name in os.listdir(input_folder): 
+        if os.path.isdir(file_name):
             continue
-        file_name= os.path.basename(fpath)
-        input_folder = os.path.dirname(os.path.realpath(fpath))
         input_file =  os.path.join(input_folder, file_name)
         steps = os.listdir(xslt_folder)
         steps.sort()
         step = None  
         for step_file in steps:
             step = step_file.split("_")[0]
-            replname = input_folder.replace(inputf,outputf)
-            output_folder = os.path.join(replname,step)
+            output_folder = os.path.join(outputf,step)
             current_step = os.path.join(output_folder, file_name)
             os.makedirs(output_folder,exist_ok=True)
             args = [
@@ -99,13 +94,14 @@ def batch_transform_xml(process_dir, variables):
                 "net.sf.saxon.Transform",
                 "-s:"+input_file,
                 "-o:"+current_step,
-                "-xsl:steps/"+step_file,
+                "-xsl:"+xslt_folder+"/"+step_file,
             ]
             if variables["parameters"]["value"]:
                 for saxon_param_strg in [key+"="+variables["parameters"]["value"][key] 
                                          for key in variables["parameters"]["value"].keys()]:
                     args.append(saxon_param_strg)
             args.append("workdir="+process_dir)
+            print(" ".join(args))
             p = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
             output, err = p.communicate()
             if p.returncode == -1:
@@ -125,7 +121,6 @@ def batch_validate_xml(process_dir, variables):
     Camunda Parameters:
         ["log-file"] -- The name of the log file containing the validation errors if any
         ["input-folder"] -- The name of the folder containing the input XML files to validate,
-                            The folder must have subfolders containing the files
         ["valid"] -- Output bool variable containing the result of the validation process
 
     """
@@ -133,8 +128,7 @@ def batch_validate_xml(process_dir, variables):
     log_file = os.path.join(process_dir, variables["log-file"]["value"])
     input_folder = os.path.join(process_dir, variables["input-folder"]["value"])
     f = open(log_file,"w")
-    glob_path = os.path.join(input_folder,'*')
-    for xml_file in glob.glob(glob_path):
+    for xml_file in os.listdir(input_folder):
         if os.path.isdir(xml_file):
             continue
         args = [
